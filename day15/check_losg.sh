87ddb0a6ed83e432125dd4364ca040cd1c7dff72
chiehiro <2320024360@campus.ouj.ac.jp>
2026-01-22 16:45:41 +0900

    new day15

---
+++ day15/check_losg.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# =====
+# ①引数チェック
+# =====
+if [ -z "$1" ] || [ -z "$2"]; then
+  echo "使い方：./check_logs.sh <log_dir> <keyword>"
+  exit 1
+fi
+
+LOG_DIR="$1"
+KEYWORD="$2"
+RESULT_FILE="result.log"
+
+# =====
+# ②ディレクトリ存在チェック
+# =====
+if [ ! -d "$LOG_DIR" ]; then
+  echo "エラー：ログディレクトリが存在しません -> $LOG_DIR"
+  exit 1
+fi
+
+# =====
+# ③.Logファイル存在チェック
+# =====
+if ! ls "$LOG_DIR"/*.log > /dev/null 2>&1; then
+  echo "エラー：log ファイルが見つかりません -> $LOG_DIR"
+  exit 1
+fi
+
+# =====
+# ④ 結果ファイル初期化
+# =====
+> "$RESULT_FILE"
+
+# =====
+# ログ判定処理
+# =====
+
